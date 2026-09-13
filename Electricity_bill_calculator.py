dd194289cd40fc9c9e755c7e8907e53c991836a8
Subham Mondal <subhammondal2502@gmail.com>
2026-09-13 08:23:49 +0530

    Add files via upload

---
+++ Electricity_bill_calculator.py
@@ -0,0 +1,5 @@
+#Electricity_bill_calculator........
+customer_name = input("enter customer_name : ")
+electricity_units = int(input("enter number of electricity units used : "))
+print("customer name : ", customer_name)
+print("units used : ", electricity_units)
