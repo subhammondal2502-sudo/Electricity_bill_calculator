93849e1a3c4962d52fa4c76f5fe3ff0ea838976c
Subham Mondal <subhammondal2502@gmail.com>
2026-09-14 08:47:27 +0530

    Reorganize electricity bill conditions .

--- Electricity_bill_calculator.py
+++ Electricity_bill_calculator.py
@@ -3,20 +3,20 @@ customer_name = input("enter customer_name : ")
 electricity_units = int(input("enter number of electricity units used : "))
 print("customer name : ", customer_name)
 print("units used : ", electricity_units)
-
 if electricity_units < 0:
     print("Invalid units")
-elif electricity_units<=100 :
-    bill = electricity_units*5
-elif electricity_units<=200 :
-    bill= (100*5)+(electricity_units- 100)*7
-elif electricity_units<=300 :
+else:
+    if electricity_units<=100 :
+        bill = electricity_units*5
+    elif electricity_units<=200 :
+        bill= (100*5)+(electricity_units- 100)*7
+    elif electricity_units<=300 :
         bill = (100*5)+(100*7)+(electricity_units-200)*10
-else:
+    else:
         bill= (100*5)+(100*7)+(100*10)+(electricity_units-300)*12    
-if bill > 2000:
+    if bill > 2000:
         surcharge = bill * 0.1
-else:
+    else:
         surcharge = 0
     print("surcharge :", surcharge)
     final_bill = bill + surcharge
