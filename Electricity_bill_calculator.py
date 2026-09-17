6fe54eda9e4313417575078e408981d8c56f21f0
Subham Mondal <subhammondal2502@gmail.com>
2026-09-17 11:45:55 +0530

    Update Electricity_bill_calculator.py

--- Electricity_bill_calculator.py
+++ Electricity_bill_calculator.py
@@ -3,6 +3,7 @@ customer_name = input("enter customer_name : ")
 electricity_units = int(input("enter number of electricity units used : "))
 print("customer name : ", customer_name)
 print("units used : ", electricity_units)
+
 if electricity_units < 0:
     print("Invalid units")
 else:
